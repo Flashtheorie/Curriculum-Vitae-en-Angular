fdfc792a1709ebdb12e9a2ac84808b2d85749a3b
Antoine Pascual <antoinepascualmail@gmail.com>
2022-04-22 23:55:26 +0200

    Ajout de la navigation "Single Page"

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent extends AppComponent{
+  override title = `CV d'Antoine Pascual`;
+  override css = `TailwindCSS`;
+
+}
