0e07fdec89a3ef4e30a6cdbd46df416daf98e82a
Antoine Pascual <antoinepascualmail@gmail.com>
2022-04-23 18:08:01 +0200

    .

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,20 +7,5 @@ import { Component, Inject, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent{
-  [x: string]: any;
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-    
-  toggleDarkMode()
-  {
-    if (localStorage['theme'] === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      this['document'].body.classList.add('dark');
-    } else {
-      this['document'].body.classList.remove('dark');
-    }
-    
-    
-  }
-  
-
-
+ 
 }
