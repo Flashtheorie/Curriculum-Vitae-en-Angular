d35faa400c17fd59e366a283ce4f7f7f287a7ab6
Antoine Pascual <antoinepascualmail@gmail.com>
2022-04-28 21:04:49 +0200

    Modification de la navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,9 +6,13 @@ import { Component, Inject, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent{
 
-  
+export class NavbarComponent{
+  showMenu: boolean | undefined;
+
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
   
   toggleDarkMode() {
     document.documentElement.classList.toggle('dark')
